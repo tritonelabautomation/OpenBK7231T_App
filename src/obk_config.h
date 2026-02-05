ce6e5643f1898161d1cb72edaef469dfafb5966a
tritonelabautomation <tritonelabautomation@gmail.com>
2026-02-05 20:33:32 +0530

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -250,7 +250,7 @@
 
 //#define	ENABLE_DRIVER_UART_TCP					1
 
-//#define	ENABLE_DRIVER_SSD1306					1
+#define	ENABLE_DRIVER_SSD1306					1
 
 // #define	ENABLE_DRIVER_PIR					1
 //#define ENABLE_DRIVER_BKPARTITIONS				1
