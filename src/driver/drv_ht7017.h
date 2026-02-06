d445c7789e840fac4c14e4f63aa39e47fc1c012a
tritonelabautomation <tritonelabautomation@gmail.com>
2026-02-06 23:33:33 +0530

    Update drv_ht7017.h

--- src/driver/drv_ht7017.h
+++ src/driver/drv_ht7017.h
@@ -14,17 +14,26 @@
 #define __HT7017_H__
 
 #include "../new_common.h"
-// Fix 1: Include HTTP definition for http_request_t
-#include "../httpserver/new_http.h" 
+#include "../httpserver/new_http.h" // Fixes unknown type http_request_t
 
-// Register Definitions [15-17]
-#define HT7017_REG_RMS_I1       0x06 
-#define HT7017_REG_RMS_U        0x08 
-#define HT7017_REG_POWER_P1     0x0A 
+// HT7017 Register Addresses (Source: Datasheet Page 16-18) [2][3]
+#define HT7017_REG_RMS_I1       0x06 // Current Channel 1 RMS
+#define HT7017_REG_RMS_U        0x08 // Voltage RMS
+#define HT7017_REG_POWER_P1     0x0A // Active Power Channel 1
 
+// Packet Constants
+#define HT7017_FRAME_HEAD       0x6A
+#define HT7017_BAUD_RATE        4800
+
+// Driver Interface
 void HT7017_Init(void);
-void HT7017_RunEverySecond(void);
-void HT7017_RunQuick(void);
+void HT7017_RunQuick(void);       
+void HT7017_RunEverySecond(void); 
 void HT7017_AppendInformationToHTTPIndexPage(http_request_t* request);
+
+// Getters
+float HT7017_GetVoltage(void);
+float HT7017_GetCurrent(void);
+float HT7017_GetPower(void);
 
 #endif // __HT7017_H__
