cf111553e8b08123de0f8e6d88d285b06f7064ce
tritonelabautomation <tritonelabautomation@gmail.com>
2026-02-24 23:08:50 +0530

    Update font_5x7.h

--- src/driver/font_5x7.h
+++ src/driver/font_5x7.h
@@ -1,12 +1,8 @@
+// font_5x7.h - BUILD FIX
+#pragma once
 #ifndef FONT_5X7_H
 #define FONT_5X7_H
 
-// 5x7 bitmap font (already embedded in your code - just formalize)
-extern const uint8_t g_font5x7[][5];
-
-#define FONT_W     5
-#define FONT_H     7  
-#define FONT_ADV   6  // Advance width (kerning)
-#define FONT_VADV  8  // Vertical advance
+extern const uint8_t g_font5x7[][5];  // ✅ Just 'extern const'
 
 #endif
