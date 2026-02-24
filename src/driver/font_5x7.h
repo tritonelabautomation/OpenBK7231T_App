bfdb785a24b9cb0355435bf35ea26b485c139578
tritonelabautomation <tritonelabautomation@gmail.com>
2026-02-24 22:36:57 +0530

    Create font_5x7.h

---
+++ src/driver/font_5x7.h
@@ -0,0 +1,12 @@
+#ifndef FONT_5X7_H
+#define FONT_5X7_H
+
+// 5x7 bitmap font (already embedded in your code - just formalize)
+extern const uint8_t g_font5x7[][5];
+
+#define FONT_W     5
+#define FONT_H     7  
+#define FONT_ADV   6  // Advance width (kerning)
+#define FONT_VADV  8  // Vertical advance
+
+#endif
