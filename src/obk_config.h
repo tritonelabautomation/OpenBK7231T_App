4e8a738a2e11300ac2fbf9c7df607b6183b91889
tritonelabautomation <tritonelabautomation@gmail.com>
2026-02-25 18:21:57 +0530

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -647,7 +647,7 @@
 #endif
 
 // if power metering chip is enabled, also enable backend for that
-#if ENABLE_DRIVER_BL0937 || ENABLE_DRIVER_BL0942 || ENABLE_DRIVER_BL0942SPI || ENABLE_DRIVER_CSE7766
+#if ENABLE_DRIVER_BL0937 || ENABLE_DRIVER_BL0942 || ENABLE_DRIVER_BL0942SPI || ENABLE_DRIVER_CSE7766 || ENABLE_DRIVER_HT7017 || ENABLE_DRIVER_ST7735
 #define ENABLE_BL_SHARED						1
 // allow use two BL0942 on two ports  +940 bytes
 // #define ENABLE_BL_TWIN						1
